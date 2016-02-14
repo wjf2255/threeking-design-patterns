45cef9bc8a2b325fcc79f4f0a875268711f4a946
wjf <123456>
2016-02-14 12:06:54 +0800

    添加异步方法调用

---
+++ java/async-method-invocation/async-method-invocations/src/main/java/com/jackframe/design_patterns/async_method_invocations/App.java
@@ -0,0 +1,13 @@
+package com.jackframe.design_patterns.async_method_invocations;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
